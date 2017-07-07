4c2f4f00208cd465e7ddf5778239b1f06f4d1244
peterjames <joyukr>
2017-07-07 19:33:05 +0300

    Initial commit

---
+++ src/main/java/vertex/lesson3/homework/Service.java
@@ -0,0 +1,13 @@
+package vertex.lesson3.homework;
+
+/**
+ * @author Igor Hnes on 7/7/17.
+ */
+public class Service {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, world");
+        
+    }
+}
